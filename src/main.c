c783b84910563c32ad10f89a58cb89f38ac0c9f4
Rich Lovely <rich@roadierich.co.uk>
2014-12-10 13:01:36 -0500

    style and fixing bugs

--- src/main.c
+++ src/main.c
@@ -21,69 +21,91 @@ static char hourStr[][7] = {
 };
 
 static void update_time(struct tm *tick_time) {
-
     
     char fuzzytext[20];
     int mins = tick_time->tm_min;
     int hour = tick_time->tm_hour;
     
+    // convert minutes to nearest five
+    
     switch(mins){
                           case  0: case  1: case  2: 
             strcpy(fuzzytext, "\n\n%s\no'clock");
             break;
+        
         case  3: case  4: case  5: case  6: case  7: 
             strcpy(fuzzytext, "\nfive\npast\n%s");
             break;
+        
         case  8: case  9: case 10: case 11: case 12: 
             strcpy(fuzzytext, "\nten\npast\n%s");
             break;
+        
         case 13: case 14: case 15: case 16: case 17: 
             strcpy(fuzzytext, "\nquarter\npast\n%s");
             break;
+        
         case 18: case 19: case 20: case 21: case 22: 
             strcpy(fuzzytext, "\ntwenty\npast\n%s");
+            break;
+        
         case 23: case 24: case 25: case 26: case 27: 
             strcpy(fuzzytext, "twenty\nfive\npast\n%s");
             break;
+        
         case 28: case 29: case 30: case 31: case 32: 
             strcpy(fuzzytext, "\nhalf\npast\n%s");
             break;
+        
         case 33: case 34: case 35: case 36: case 37: 
             strcpy(fuzzytext, "twenty\nfive\nto\n%s");
             hour += 1;
             break;
+        
         case 38: case 39: case 40: case 41: case 42: 
             strcpy(fuzzytext, "\ntwenty\nto\n%s");
             hour += 1;
             break;
+        
         case 43: case 44: case 45: case 46: case 47: 
             strcpy(fuzzytext, "\nquarter\nto\n%s");
             hour += 1;
             break;
+        
         case 48: case 49: case 50: case 51: case 52: 
             strcpy(fuzzytext, "\nten\nto\n%s");
             hour += 1;
             break;
+        
         case 53: case 54: case 55: case 56: case 57: 
-            strcpy(fuzzytext, "five\nto\n%s");
+            strcpy(fuzzytext, "\nfive\nto\n%s");
             hour += 1;
             break;
+        
         case 58: case 59:
             strcpy(fuzzytext, "\n\n%s\no'clock");
             hour += 1;
             break;
+        
         default:
             strcpy(fuzzytext, "invalid time");
             break;
     }
     
+    // AM/PM is irrelevant
     if(hour > 12)
     {
         hour -= 12;
     }
+    else if (hour == 0)
+    {
+        hour = 12;
+    }
     
+    // get text representation of hour
     char * hourText = hourStr[hour-1];
     
+    // combine minute and hour
     snprintf(buffer, strlen(fuzzytext)+strlen(hourText), fuzzytext, hourText);
   
     // Display this time on the TextLayer
@@ -97,8 +119,8 @@ static void main_window_load(Window *window) {
     text_layer_set_text_color(s_time_layer, GColorWhite);
     text_layer_set_text(s_time_layer, "00:00");
 
-    // Improve the layout to be more like a watchface
-    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+    // Format text layer
+    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
     text_layer_set_text_alignment(s_time_layer, GTextAlignmentRight);
     text_layer_set_overflow_mode(s_time_layer, GTextOverflowModeWordWrap);
 
